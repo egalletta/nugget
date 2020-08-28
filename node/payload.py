bf5d14ce33971541fd1429f4e5c5284706636dcb
Enzo E. Galletta <enzo@galletta.xyz>
2020-08-27 23:57:06 -0400

    add get request to payload

--- node/payload.py
+++ node/payload.py
@@ -1,5 +1,10 @@
 def run():
-    print('Hello World!')
+    import urequests
+    from time import sleep
+    while True:
+        req = urequests.get('http://n-u-g-g-e-t.herokuapp.com/motd')
+        print(req.text)
+        sleep(5)
 
 if __name__ == '__main__':
     run()
