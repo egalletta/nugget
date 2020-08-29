c60d5e78ff6cae9041f41bbafb875bdb6a3095e5
Enzo E. Galletta <enzo@galletta.xyz>
2020-08-29 01:26:04 -0400

    add lcd support

--- node/payload.py
+++ node/payload.py
@@ -1,10 +1,28 @@
+from machine import Pin
+from utime import sleep, ticks_ms
+from nodemcu_gpio_lcd import GpioLcd
+import urequests
+from time import sleep
+
+lcd = GpioLcd(rs_pin=Pin(16),
+                  enable_pin=Pin(5),
+                  d4_pin=Pin(4),
+                  d5_pin=Pin(0),
+                  d6_pin=Pin(2),
+                  d7_pin=Pin(14),
+                  num_lines=2, num_columns=16)
+
 def run():
-    import urequests
-    from time import sleep
     while True:
         req = urequests.get('http://n-u-g-g-e-t.herokuapp.com/motd')
-        print(req.text)
+        lprint(req.text)
         sleep(5)
+
+def lprint(s):
+    print(s)
+    lcd.clear()
+    sleep(0.15)
+    lcd.putstr(s)
 
 if __name__ == '__main__':
     run()
--- webservice/app.py
+++ webservice/app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from flask_pymongo import PyMongo
+import os
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get('MONGOSTR')
+mongo = PyMongo(app)
 
 @app.route('/motd', methods=['GET'])
 def hello_world():
-   return "Hello World (from the cloud!)!"
+    filt = {'target' : 'node-nora'}
+    motd = mongo.db.messages.find_one(filt)
+    return motd['motd']
+    
+
+@app.route('/update', methods=['PUT'])
+def update():
+    new_message = request.get_json()
+    filt = {'target' : 'node-nora'}
+    to_write = {"$set": {"motd": new_message['motd']}}
+    mongo.db.messages.update_one(filt, to_write)
+    result = {'result' : 'Updated successfully'}
+    return result
 
 if __name__ == '__main__':
    app.run()
