bce510b91c89bc5938cd126e3e609b2de4f5dad4
Enzo E. Galletta <enzo@galletta.xyz>
2020-08-30 01:12:25 -0400

    add lcd boot messages

--- node/boot.py
+++ node/boot.py
@@ -29,6 +29,13 @@ def do_connect():
     print('network config:', sta_if.ifconfig())
 
 def lprint(s):
+    lcd = GpioLcd(rs_pin=Pin(16),
+                  enable_pin=Pin(5),
+                  d4_pin=Pin(4),
+                  d5_pin=Pin(0),
+                  d6_pin=Pin(2),
+                  d7_pin=Pin(14),
+                  num_lines=2, num_columns=16)
     print(s)
     lcd.clear()
     sleep(0.15)
--- node/payload.py
+++ node/payload.py
@@ -5,19 +5,11 @@ import urequests
 from time import sleep
 from boot import lprint
 
-lcd = GpioLcd(rs_pin=Pin(16),
-                  enable_pin=Pin(5),
-                  d4_pin=Pin(4),
-                  d5_pin=Pin(0),
-                  d6_pin=Pin(2),
-                  d7_pin=Pin(14),
-                  num_lines=2, num_columns=16)
-
 def run():
     while True:
         req = urequests.get('http://n-u-g-g-e-t.herokuapp.com/motd')
         lprint(req.text)
-        sleep(5)
+        sleep(5
 
 if __name__ == '__main__':
     run()
