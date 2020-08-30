51866fc43ea606ffe02f8d4cc229363f47a5c2e9
Enzo E. Galletta <enzo@galletta.xyz>
2020-08-30 01:27:22 -0400

    debugging boot

--- node/payload.py
+++ node/payload.py
@@ -8,5 +8,18 @@ def start():
         lprint(req.text)
         sleep(5)
 
+def lprint(s):
+    lcd = GpioLcd(rs_pin=Pin(16),
+                  enable_pin=Pin(5),
+                  d4_pin=Pin(4),
+                  d5_pin=Pin(0),
+                  d6_pin=Pin(2),
+                  d7_pin=Pin(14),
+                  num_lines=2, num_columns=16)
+    print(s)
+    lcd.clear()
+    sleep(0.15)
+    lcd.putstr(s)
+
 if __name__ == '__main__':
     run()
