7d630310d07b723bdc7fe7e533cd6e495ec33a08
Enzo E. Galletta <enzo@galletta.xyz>
2020-09-16 20:20:29 -0400

    fix I2C typo

--- node/boot.py
+++ node/boot.py
@@ -45,7 +45,7 @@ def lprint(s):
                     d7_pin=Pin(14),
                     num_lines=2, num_columns=16)
     except (ImportError, OSError):
-        from nodemcu_gpio_lcd import I2CLcd
+        from nodemcu_gpio_lcd import I2cLcd
         i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
         lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
     print(s)
