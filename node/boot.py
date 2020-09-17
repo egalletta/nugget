b6c4eb4b1c270f85318de53086c624c843cf848a
Enzo E. Galletta <enzo@galletta.xyz>
2020-09-16 20:25:24 -0400

    import fix

--- node/boot.py
+++ node/boot.py
@@ -46,6 +46,7 @@ def lprint(s):
                     num_lines=2, num_columns=16)
     except (ImportError, OSError):
         from nodemcu_gpio_lcd import I2cLcd
+        from machine import I2C
         i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
         lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
     print(s)
