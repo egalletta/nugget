21b055b7d4082e61a71e7010142787c3b77b29e0
Enzo E. Galletta <enzo@galletta.xyz>
2020-09-16 20:27:55 -0400

    i2C addr

--- node/main.py
+++ node/main.py
@@ -23,6 +23,8 @@ def lprint(text: str, delay: float):
     except (ImportError, OSError):
         from nodemcu_gpio_lcd import I2cLcd
         from machine import I2C
+        # The PCF8574 has a jumper selectable address: 0x20 - 0x27
+        DEFAULT_I2C_ADDR = 0x27
         i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
         lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
     if len(text) <= 32:
